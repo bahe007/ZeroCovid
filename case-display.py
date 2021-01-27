995fa727ec995e81a368793d8c1876ce255f4825
Bastin Heinlein <bastian.heinlein@live.de>
2021-01-27 21:52:10 +0100

    Case-Display ist jetzt auch auf automatische Updates vorbereitet

--- case-display.py
+++ case-display.py
@@ -16,11 +16,13 @@ fig, ax = plt.subplots()
 ax.plot(t[:-3], helpers.moving_average(cases)[:-3])
 
 # Achsenbeschritung
-xlabels = ["1. Februar", "1. April", "1. Juni", "1. August", "1. Oktober", "1. Dezember"]
-ax.set_xticks(["2020/02/01", "2020/04/01", "2020/06/01", "2020/08/01", "2020/10/01", "2020/12/01"])
+xlabels = ["1. Februar", "1. April", "1. Juni", "1. August", "1. Oktober", "1. Dezember", "1. Februar"]
+ax.set_xticks(["2020/02/01", "2020/04/01", "2020/06/01", "2020/08/01", "2020/10/01", "2020/12/01", "2021/02/01"])
 ax.set_xticklabels(xlabels)
 ax.grid(True)
 
-ax.set_ylabel("Tägliche Neuinfektionen (geglättet)")
+ax.set_ylabel("Meldedaten des RKI (geglättet)")
 
-plt.show()
+fig.set_figheight(9.6)
+fig.set_figwidth(15)
+plt.savefig("images/daily-new-cases.png")
