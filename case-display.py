1f28fcf0306a6fde6c221b9925638e37513b38b8
Bastin Heinlein <bastian.heinlein@live.de>
2021-03-11 20:44:09 +0100

    Fallzahlen-Darstellung zeigt nun auch die tatsächlich gemeldeten Werte an
    Zusätzlich zu den gemittelten Werten werden nun mit einem Scatter-Plot auch die jeweiligen Tagesmeldungen angezeigt.

--- case-display.py
+++ case-display.py
@@ -13,11 +13,12 @@ t, cases = case_numbers.get_cases(file_name)
 ############## Zeichne Fallzahlen ##############
 fig, ax = plt.subplots()
 
-ax.plot(t[:-3], helpers.moving_average(cases)[:-3], color="black")
+ax.scatter(t[:], cases[:], color="grey", s=2, label="Tagesmeldungen")
+ax.plot(t[:-3], helpers.moving_average(cases)[:-3], color="black", label="Gemittelte Werte")
 
 # Achsenbeschritung
-xlabels = ["1. Februar", "1. April", "1. Juni", "1. August", "1. Oktober", "1. Dezember", "1. Februar"]
-ax.set_xticks(["2020/02/01", "2020/04/01", "2020/06/01", "2020/08/01", "2020/10/01", "2020/12/01", "2021/02/01"])
+xlabels = ["1. Februar", "1. April", "1. Juni", "1. August", "1. Oktober", "1. Dezember", "1. Februar", "1. März"]
+ax.set_xticks(["2020/02/01", "2020/04/01", "2020/06/01", "2020/08/01", "2020/10/01", "2020/12/01", "2021/02/01", "2021/03/01"])
 ax.set_xticklabels(xlabels)
 ax.grid(True)
 
@@ -26,4 +27,5 @@ ax.set_ylabel("Meldedaten des RKI (geglättet)")
 fig.set_figheight(9.6)
 fig.set_figwidth(15)
 plt.title("Aktualisiert am {} um 08:00 UTC".format(helpers.todays_date()))
+plt.legend(loc="upper left")
 plt.savefig("images/daily-new-cases.png")
